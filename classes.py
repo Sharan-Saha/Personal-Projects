ada2539cc5ca86aa44ac8506380daa6fa065df66
Sharan Saha <sharansaha@Sharans-MacBook-Air.local>
2021-10-12 19:18:38 -0400

    Commit message

--- classes.py
+++ classes.py
@@ -8,6 +8,9 @@ class Person:
     
     def description(self):
         print(f"{self.name} is {self.age} years old and is a {self.human_type}")
+    
+    def speak(self, word):
+        print(f"{self.name} says {word}")
 
         
 sharan = Person("Sharan", 17)
@@ -16,6 +19,20 @@ print(sharan.name)
 print(sharan.age)
 print(sharan.human_type)
 sharan.description()
+sharan.speak("Hello")
 
 clay = Person("Clay", 18)
 clay.description()
+
+class Car:
+    type = "Vehicle"
+    def __init__(self, color, miles):
+        self.color = color
+        self.miles = miles
+
+    def getDescription(self):
+        print(f"The {self.color} car has {self.miles} miles")
+
+
+car1 = Car("White", 50000)
+car1.getDescription()
